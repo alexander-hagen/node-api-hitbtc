b2f7448e40eeab7ece5cec63005250690036bbd6
yuma300 <ywakimoto2s@gmail.com>
2018-01-19 18:08:52 +0900

    implement for public APIs

--- example/example.js
+++ example/example.js
@@ -1,4 +1,8 @@
-var bitbank = require("../");
+var hitbtc = require("../");
 
-var api = bitbank.publicApi();
+var api = hitbtc.publicApi();
 api.getOrderBook("ETHBTC").then(console.log);
+api.getTicker("ETHBTC").then(console.log);
+api.getTrades("ETHBTC", "DESC").then(console.log);
+api.getTrades("ETHBTC").then(console.log);
+api.getCandlestick("ETHBTC", "M30").then(console.log);
